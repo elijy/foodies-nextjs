4df1e2165eab102a1c3593160ddadd70b6f95f65
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-04-02 14:12:12 -0400

    move header components around

---
+++ components/header/header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import HeaderBackground from "./header-background";
+import logo from "@/assets/logo.png";
+import classes from "./header.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <HeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logo} alt="a plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
