b27587ca93caec8e14d7cb5556f69181e3773257
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-04-03 18:05:42 -0400

    nav link component

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
+
 import HeaderBackground from "./header-background";
+import NavLink from "./nav-link";
+
 import logo from "@/assets/logo.png";
 import classes from "./header.module.css";
 
@@ -17,10 +20,10 @@ export default function Header() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
