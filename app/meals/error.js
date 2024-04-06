a5426cf1a2b690e87b2bbbd537ab0b03b46b6be6
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-04-06 12:50:32 -0400

    add error page

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>Ruh Roh</h1>
+      <p>Something went wrong :/</p>
+    </main>
+  );
+}
